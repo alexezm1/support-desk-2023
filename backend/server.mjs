b96194afc5c00120d9c5fc29aaa6486a4832a106
alexezm1 <alex_speed_96@hotmail.com>
2023-11-09 14:27:31 -0700

    error handling middleware

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { userRoutes } from "./routes/userRoutes.mjs";
+import { errorHandler } from "./middleware/errorHandler.mjs";
 
 dotenv.config();
 
@@ -8,7 +9,12 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // User Routes
 app.use("/api/users", userRoutes);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`server started at port ${PORT}`));
