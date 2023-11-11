2a4afd565b150605d8d2ddfc0dba7ec70bf983a6
alexezm1 <alex_speed_96@hotmail.com>
2023-11-11 12:35:50 -0700

    updated queries

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -30,15 +30,18 @@ const registerUser = async (req, res) => {
       res.status(400);
       throw new Error("Fill all the required fields");
     }
+
     // Check if user already exists
     const results = await pool.query(checkUserEmails, [email]);
     if (results.rows.length) {
       res.status(400);
       throw new Error("User already exists");
     }
+
     // Hash password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
+
     // Add user
     const user = await pool.query(addUser, [
       name,
@@ -47,6 +50,7 @@ const registerUser = async (req, res) => {
       email,
       timestamp,
     ]);
+
     if (user) {
       res.status(201).json({
         message: "User Added Succesfullt",
--- backend/queries/userQueries.js
+++ backend/queries/userQueries.js
@@ -1,5 +1,5 @@
 const checkUserEmails = `SELECT * FROM users AS u WHERE u.email = $1`;
 const getUsersData = `SELECT * FROM users`;
-const addUser = `INSERT INTO users (name, salt, password, email, timestamp, date) VALUES ($1, $2, $3, $4, $5, to_timestamp(${Date.now()} / 1000.0))`;
+const addUser = `INSERT INTO users (name, salt, password, email, timestamp) VALUES ($1, $2, $3, $4, $5)`;
 
 module.exports = { checkUserEmails, addUser, getUsersData };
