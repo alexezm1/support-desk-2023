88f384f51dcd3d3cd9b5e7bb5db4ace16853d288
alexezm1 <alex_speed_96@hotmail.com>
2023-11-13 08:40:12 -0700

    updated query

--- backend/queries/database.sql
+++ backend/queries/database.sql
@@ -5,9 +5,8 @@ CREATE TABLE IF NOT EXISTS users(
 	password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     isAdmin BOOLEAN DEFAULT FALSE,
-    date DATE NOT NULL,
     timestamp TIMESTAMP NOT NULL
 )
 
 SELECT * FROM users AS u WHERE u.email = $1
-INSERT INTO users (name, password, email, timestamp, date) VALUES ($1, $2, $3, $4, $5)
+INSERT INTO users (name, salt, password, email, timestamp) VALUES ($1, $2, $3, $4, $5)
