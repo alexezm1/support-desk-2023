b7df7505d7ee89dccbc49cbe383b2eb370dcf755
alexezm1 <alex_speed_96@hotmail.com>
2023-11-13 13:02:17 -0700

    toast container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
@@ -8,17 +10,20 @@ import Register from "./pages/Register";
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <Header />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/sign-up" element={<Register />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-        {/* <Footer /> */}
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/sign-up" element={<Register />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+          {/* <Footer /> */}
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
