30dc3d4f17712838b96094738503cac254547c70
alexezm1 <alex_speed_96@hotmail.com>
2023-11-13 09:22:57 -0700

    async handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,23 +5,24 @@ const {
   addUser,
   getUsersData,
 } = require("../queries/userQueries.js");
+const AsyncHandler = require("express-async-handler");
 
 // @desc GET users
 // @route Route /api/users
 // @access Public
-const getUsers = async (req, res) => {
+const getUsers = AsyncHandler(async (req, res) => {
   try {
     const results = await pool.query(getUsersData);
     res.status(200).json(results.rows);
   } catch (error) {
     throw new Error("Could not get any users");
   }
-};
+});
 
 // @desc Register new user
 // @route Route /api/users
 // @access Public
-const registerUser = async (req, res) => {
+const registerUser = AsyncHandler(async (req, res) => {
   try {
     const { name, email, password } = req.body;
     const timestamp = new Date(Date.now()).toISOString();
@@ -53,7 +54,7 @@ const registerUser = async (req, res) => {
 
     if (user) {
       res.status(201).json({
-        message: "User Added Succesfullt",
+        message: "User Added Succesfully",
         body: {
           user: { name, email },
         },
@@ -65,14 +66,30 @@ const registerUser = async (req, res) => {
   } catch (error) {
     console.log(error);
   }
-};
+});
 
 // @desc Login user
 // @route Route /api/users/login
 // @access Public
-const loginUser = async (req, res) => {
-  res.send("Login Route");
-};
+const loginUser = AsyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await pool.query(checkUserEmails, [email]);
+  const passwordMatch = await bcrypt.compare(password, user.rows[0].password);
+
+  // Check user and passwords match
+  if (user.rows[0].email && passwordMatch) {
+    res.status(200).json({
+      message: "Login Successfull",
+      body: {
+        user: { name: user.rows[0].name, email: user.rows[0].email },
+      },
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid credentials");
+  }
+});
 
 module.exports = {
   registerUser,
