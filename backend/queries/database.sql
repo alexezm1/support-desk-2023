08698f753f6e0cccd092c05df297b443559f57dd
alexezm1 <alex_speed_96@hotmail.com>
2023-11-16 12:28:33 -0700

    ticket and product model

--- backend/queries/database.sql
+++ backend/queries/database.sql
@@ -8,5 +8,21 @@ CREATE TABLE IF NOT EXISTS users(
     timestamp TIMESTAMP NOT NULL
 )
 
+CREATE TABLE IF NOT EXISTS tickets(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES users,
+    product_id UUID REFERENCES products,
+	description VARCHAR(255) NOT NULL,
+	status VARCHAR(255) NOT NULL,
+    timestamp TIMESTAMP NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS products (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    price NUMERIC NOT NULL,
+    timestamp TIMESTAMP NOT NULL
+)
+
 SELECT * FROM users AS u WHERE u.email = $1
 INSERT INTO users (name, salt, password, email, timestamp) VALUES ($1, $2, $3, $4, $5)
