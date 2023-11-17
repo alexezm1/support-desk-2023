a795b6d3a4a141099c80a66f0f94a7b96ad8d20c
alexezm1 <alex_speed_96@hotmail.com>
2023-11-16 18:54:40 -0700

    corrected WHERE clause in get user tickets query

--- backend/queries/userQueries.js
+++ backend/queries/userQueries.js
@@ -5,7 +5,7 @@ const getUsersData = `SELECT * FROM users`;
 const addUser = `INSERT INTO users (name, salt, password, email, timestamp) VALUES ($1, $2, $3, $4, $5)`;
 
 // TICKET QUERIES
-const getUserTicketsFromDB = "SELECT * FROM tickets WHERE id = $1";
+const getUserTicketsFromDB = "SELECT * FROM tickets WHERE user_id = $1";
 const createTicket =
   "INSERT INTO tickets (user_id, product, description, timestamp) VALUES ($1, $2, $3, $4) RETURNING id, user_id, timestamp";
 
