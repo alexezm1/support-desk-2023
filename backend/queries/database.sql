d4fbddb8145f0340ad03e7dc4e83372881643192
alexezm1 <alex_speed_96@hotmail.com>
2023-11-16 18:45:10 -0700

    create ticket and get tickets from user queries and get products query

--- backend/queries/database.sql
+++ backend/queries/database.sql
@@ -11,18 +11,20 @@ CREATE TABLE IF NOT EXISTS users(
 CREATE TABLE IF NOT EXISTS tickets(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID REFERENCES users,
-    product_id UUID REFERENCES products,
+    product VARCHAR(255) NOT NULL,
 	description VARCHAR(255) NOT NULL,
-	status VARCHAR(255) NOT NULL,
+	status VARCHAR(255) NOT NULL DEFAULT 'New',
     timestamp TIMESTAMP NOT NULL
 )
 
 CREATE TABLE IF NOT EXISTS products (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(255) NOT NULL,
-    price NUMERIC NOT NULL,
+    price NUMERIC(12,2) NOT NULL,
     timestamp TIMESTAMP NOT NULL
 )
 
 SELECT * FROM users AS u WHERE u.email = $1
 INSERT INTO users (name, salt, password, email, timestamp) VALUES ($1, $2, $3, $4, $5)
+INSERT INTO tickets (user_id, product_id, description, status, timestamp) VALUES ($1, $2, $3, $4, $5)
+INSERT INTO products (name, price, timestamp) VALUES ('Iphone', 150.99, '2023-11-14T00:36:40.523Z')
--- backend/queries/userQueries.js
+++ backend/queries/userQueries.js
@@ -1,6 +1,24 @@
+// USER QUERIES
 const checkUserEmail = `SELECT * FROM users AS u WHERE u.email = $1`;
 const checkUserID = `SELECT * FROM users AS u WHERE u.id = $1`;
 const getUsersData = `SELECT * FROM users`;
 const addUser = `INSERT INTO users (name, salt, password, email, timestamp) VALUES ($1, $2, $3, $4, $5)`;
 
-module.exports = { checkUserEmail, addUser, getUsersData, checkUserID };
+// TICKET QUERIES
+const getUserTicketsFromDB = "SELECT * FROM tickets WHERE id = $1";
+const createTicket =
+  "INSERT INTO tickets (user_id, product, description, timestamp) VALUES ($1, $2, $3, $4)";
+
+// PRODUCT QUERIES
+const getProducts = "SELECT id, name FROM products";
+const createProduct = "";
+
+module.exports = {
+  checkUserEmail,
+  addUser,
+  getUsersData,
+  checkUserID,
+  getUserTicketsFromDB,
+  createTicket,
+  getProducts,
+};
