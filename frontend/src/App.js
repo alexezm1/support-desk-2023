f89bfdfc527ed00f618b8842870d39643b140319
alexezm1 <alex_speed_96@hotmail.com>
2023-11-22 10:00:27 -0700

    Private Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import NewTicket from "./pages/NewTicket";
 import Register from "./pages/Register";
 
 function App() {
@@ -18,6 +20,14 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/sign-up" element={<Register />} />
             <Route path="/" element={<Home />} />
+            <Route
+              path="/new-ticket"
+              element={
+                <PrivateRoute>
+                  <NewTicket />
+                </PrivateRoute>
+              }
+            />
           </Routes>
           {/* <Footer /> */}
         </div>
