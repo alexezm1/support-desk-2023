bf30612a0bd200ba0329e638042aa537d38eb608
alexezm1 <alex_speed_96@hotmail.com>
2023-12-01 18:26:33 -0700

    added note reducers

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "../features/auth/authSlice";
+import noteSlice from "../features/notes/noteSlice";
 import productSlice from "../features/products/productSlice";
 import ticketSlice from "../features/tickets/ticketSlice";
 
 export const store = configureStore({
-  reducer: { auth: authSlice, ticket: ticketSlice, products: productSlice },
+  reducer: {
+    auth: authSlice,
+    ticket: ticketSlice,
+    notes: noteSlice,
+    products: productSlice,
+  },
 });
